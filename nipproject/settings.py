714e05687ab9b8ae1e2209f7c67ffa7930eabd12
vikash <vikash.kumar979@gmail.com>
2020-06-09 01:18:45 +0530

    Change titles and styling on homepage

--- nipproject/settings.py
+++ nipproject/settings.py
@@ -91,10 +91,31 @@ DATE_INPUT_FORMATS = ("%d-%m-%Y"),
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'navynip',
+        'USER': 'postgres',
+        'PASSWORD': 'sudha007',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
+    'users': {
+        'NAME': 'users',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': ''
     }
 }
 
